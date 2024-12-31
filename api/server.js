ca29efc3400f57cb3f7480399462c6f4b72dfd22
joshlevylabs <josh@joshlevylabs.com>
2024-12-31 13:17:22 -0800

    adjusted serve static files from public directory instead of api

--- api/server.js
+++ api/server.js
@@ -13,7 +13,7 @@ app.use(cors());
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 // Serve static files directly from the root directory (for Vercel)
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Debugging middleware to log all incoming requests
 app.use((req, res, next) => {
@@ -21,9 +21,9 @@ app.use((req, res, next) => {
     next();
 });
 
-// Handle PDF and static file requests with detailed logging
+// Serve static files like PDFs from 'public' directory
 app.get('/:filename', (req, res) => {
-    const filePath = path.join(__dirname, req.params.filename);
+    const filePath = path.join(__dirname, 'public', req.params.filename);
 
     console.log(`📂 Attempting to serve file: ${req.params.filename}`);
     console.log(`🔍 Resolved file path: ${filePath}`);
